4fe0665f267865ac1837fdf9abff78e36d0f12d3
Manish Thapa Chhetri <xetrimohan12@gmail.com>
2024-12-22 02:17:00 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GreetingElement from './myGreetingApp' 
+import AppColor from './AppbackgroundColor'
+ import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+   <AppColor heading="This is first element" lbl=
+"Name :"  color="green"/>
+   <AppColor heading="This is second element" lbl
+="Name :"  color="blue"/>
+   <AppColor heading="This is third third element
+" lbl="Name :"  color="Yellow"/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
